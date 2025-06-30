781de2074200ec7c34a3efe4b257d1eb92a4fa18
Harshdeep Athawale <harshdeepathawale777@gmail.com>
2025-06-30 19:44:32 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -105,7 +105,7 @@ export default function Page() {
           <section className="mb-20 max-w-3xl">
             <h2 className="text-xl font-medium mb-6 text-gray-600 dark:text-gray-400">about me.</h2>
             <p className="text-gray-700 dark:text-gray-300 leading-relaxed">
-              Hey! I'm a cybersecurity enthusiast with a keen interest in ethical hacking, penetration testing, and
+              Hey! I'm a cybersecurity enthusiast with a keen interest in ethical hacking, Cloud Computing, and
               network security. I love diving into networks, analyzing traffic, and using tools to uncover hidden
               insights. I'm currently studying Computer Engineering at Thapar Institute while actively contributing to the OWASP
               Student Chapter.
